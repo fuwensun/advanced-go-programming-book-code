64dd4c4235fbae65400f427f8fd4bd89e1d12106
sfw <sfuwen@sina.com>
2018-07-06 19:29:23 +0800

    --ch4-01b--

--- ch4/rpc01/cs01/rpcClient01/client.go
+++ ch4/rpc01/cs01/rpcClient01/client.go
@@ -14,11 +14,12 @@ func main() {
 	}
 
 	var reply string
-	//Client 远程调用HelloService.Hello方法，参数"hello"
+	//Client 远程调用HelloService.Hello方法，输入参数"hello"，输出参数&reply
 	err = client.Call("HelloService.Hello", "hello", &reply)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//打印回复
 	fmt.Println(reply)
 }

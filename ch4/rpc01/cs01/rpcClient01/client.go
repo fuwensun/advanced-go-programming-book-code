c0b32166d87a192806451b2209f2925703be7eab
sfw <sfuwen@sina.com>
2018-07-06 19:09:06 +0800

    --ch4-01a--

--- ch4/rpc01/cs01/rpcClient01/client.go
+++ ch4/rpc01/cs01/rpcClient01/client.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
+	//rpc 连线，生成*Client
 	client, err := rpc.Dial("tcp", "localhost:1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
 
 	var reply string
+	//Client 远程调用HelloService.Hello方法，参数"hello"
 	err = client.Call("HelloService.Hello", "hello", &reply)
 	if err != nil {
 		log.Fatal(err)
--- ch4/rpc01/cs01/rpcServer01/server.go
+++ ch4/rpc01/cs01/rpcServer01/server.go
@@ -15,17 +15,22 @@ func (p *HelloService) Hello(request string, reply *string) error {
 
 
 func main() {
+
+	//rpc 注册
 	rpc.RegisterName("HelloService", new(HelloService))
 
+	//*TCPListener 建立
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("ListenTCP error:", err)
 	}
 
+	//*TCPConn  获取
 	conn, err := listener.Accept()
 	if err != nil {
 		log.Fatal("Accept error:", err)
 	}
-
+	
+	//rpc绑定
 	rpc.ServeConn(conn)
 }

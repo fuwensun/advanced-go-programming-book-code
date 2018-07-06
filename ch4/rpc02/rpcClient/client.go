a5feac32fec6b708409bac4a43b8d6cbc0fd926b
sfw <sfuwen@sina.com>
2018-07-06 20:06:29 +0800

    refactor

---
+++ ch4/rpc02/rpcClient/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/rpc"
+	"log"
+	"fmt"
+)
+
+func main() {
+	//rpc 连线，生成*Client
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	var reply string
+	//Client 远程调用HelloService.Hello方法，输入参数"hello"，输出参数&reply
+	//err = client.Call("HelloService.Hello", "hello", &reply)	//v1
+	err = client.Call("path/to/pkg.HelloService"+".Hello", "hello", &reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//打印回复
+	fmt.Println(reply)
+}
---
+++ ch4/rpc02/rpcServer/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/rpc"
+	"net"
+	"log"
+)
+
+//服务接口定义
+const HelloServiceName = "path/to/pkg.HelloService"
+
+type HelloServiceInterface = interface {
+	Hello(request string, reply *string) error
+}
+
+func RegisterHelloService(svc HelloServiceInterface) error {
+	return rpc.RegisterName(HelloServiceName, svc)
+}
+
+//服务类型定义
+type HelloService struct {}
+
+func (p *HelloService) Hello(request string, reply *string) error {
+	*reply = "hello:" + request
+	return nil
+}
+
+
+func main() {
+
+	//rpc 注册
+	//rpc.RegisterName("HelloService", new(HelloService)) //v1
+	RegisterHelloService(new(HelloService))
+
+	//*TCPListener 建立
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("ListenTCP error:", err)
+	}
+
+	//*TCPConn  获取
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("Accept error:", err)
+	}
+	
+	//rpc绑定
+	rpc.ServeConn(conn)
+}
